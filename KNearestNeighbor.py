df3266525f33ed65354f54011123cc473f9fa7bb
seshu_thota <seshu@capiot.com>
2019-06-06 18:34:40 +0530

    Reformatted code and created class and methods for easier use.

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -11,11 +11,11 @@ for all the points.But it can be multi threaded to make it much faster
 
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-import warnings
 from collections import Counter
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -27,25 +27,52 @@ dataset = {"k": [[1, 2], [2, 3], [3, 4]], "r": [[6, 5], [7, 8], [8, 9]]}
 new_features = [5, 7]
 
 
-def k_nearest_neighbor(data, predict, k):
-    if len(data) >= k:
-        warnings.warn("K should not be bigger than the length of the data")
-    distances = []
-    for group in data:
-        for features in data[group]:
-            eucledian_distance = np.linalg.norm(np.array(features) - np.array(predict))
-            distances.append([eucledian_distance, group])
-    votes = [i[1] for i in sorted(distances)[:k]]
-    vote_result = Counter(votes).most_common(1)[0][0]
-    confidence = Counter(votes).most_common(1)[0][1]/k
-    return vote_result, confidence
+class K_nearest_neighbor:
 
+    def __init__(self, k):
+        self.k = k
+        self.train_set = {}
 
-# result = k_nearest_neighbor(dataset, new_features, 3)
-#
-# [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-# plt.scatter(new_features[0], new_features[1], color=result, marker='*')
-# plt.show()
+    def train(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+        for _ in np.unique(y_train):
+            self.train_set[_] = []
+        for i in range(len(X_train)):
+            self.train_set[y_train[i]].append(X_train[i])
+
+    def predict(self, X_test):
+        distances = []
+        for group in self.train_set:
+            for features in self.train_set[group]:
+                eucledian_distance = np.linalg.norm(np.array(features) - np.array(X_test))
+                distances.append([eucledian_distance, group])
+        votes = [i[1] for i in sorted(distances)[:self.k]]
+        vote_result = Counter(votes).most_common(1)[0][0]
+        confidence = Counter(votes).most_common(1)[0][1] / self.k
+        return vote_result, confidence
+
+    def accuracy(self, X_test, y_test):
+        test_set = {}
+        for _ in np.unique(y_test):
+            test_set[_] = []
+        for i in range(len(y_test)):
+            test_set[y_test[i]].append(X_test[i])
+
+        vote_result_list = []
+        confidence_list = []
+        correct = 0.0
+        total_count = 0.0
+        for _ in test_set:
+            for features in test_set[_]:
+                vote_result, confidence = self.predict(features)
+                if vote_result == _:
+                    correct += 1
+                total_count += 1
+                vote_result_list.append([vote_result, confidence])
+                confidence_list.append(confidence)
+        accuracy = correct / total_count
+        return accuracy
 
 
 breast_cancer = datasets.load_breast_cancer()
@@ -65,15 +92,17 @@ for _ in range(len(y_test)):
 
 correct = 0
 total = 0.0
+k = [int(x) for x in range(1, 51)]
+Accuracy = []
+for i in k:
+    clf2 = K_nearest_neighbor(k=i)
+    clf2.train(X_train, y_train)
+    accuracy = clf2.accuracy(X_test, y_test)
+    Accuracy.append(accuracy)
+    print("Accuracy from KNN for {} is {} ".format(i, accuracy))
 
-for group in test_set:
-    for data in test_set[group]:
-        vote, confidence = k_nearest_neighbor(train_set, data, 5)
-        if group == vote:
-            correct += 1
-        total += 1
-print("Accuracy : %0.4f " % (correct / total))
-
-clf = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
+clf = KNeighborsClassifier()
 clf.fit(X_train, y_train)
 print("Accuracy from sklkearn : %0.4f " % (clf.score(X_test, y_test)))
+plt.plot(k, Accuracy)
+plt.show()
